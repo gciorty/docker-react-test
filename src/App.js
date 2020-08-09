cad5dfb01508cfe2f726b87509004b10abbce496
Gabriel Ciortea Pop <gabriel@Gabriels-MBP.lan>
2020-08-09 11:05:23 +0100

    feature ranch test

--- src/App.js
+++ src/App.js
@@ -7,7 +7,9 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>Hi There! I am React, a front-end framework!</p>
+        <p>
+          Hi There! I am React, a front-end framework! Changed on feature branch
+        </p>
         <a
           className="App-link"
           href="https://reactjs.org"
